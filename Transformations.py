626275352e396740a69a0c89966bb59ab3d8590d
brunolobell <brunolobell2@gmail.com>
2021-11-08 00:08:46 -0300

    Novas funções de transformacao

--- Transformations.py
+++ Transformations.py
@@ -21,3 +21,25 @@ def scale(newScale):
 def translate(x, y, z):
     T = [[1,0,0,x],[0,1,0,y],[0,0,1,z],[0,0,0,1]]
     glMultMatrixf(np.transpose(T))
+
+def orthogonalProjection(left, right, bottom, top, near, far):
+    p1 = 2.0 / (right - left)
+    p2 = 2.0 / (top - bottom)
+    p3 = -2.0 / (far - near)
+    p4 = -(right + left) / (right - left)
+    p5 = -(top + bottom) / (top - bottom)
+    p6 = -(far + near) / (far - near)
+    P = [[p1, 0, 0, 0], [0, p2, 0, 0], [0, 0, p3, 0], [p4, p5, p6, 1]]
+    glMultMatrixf(P)
+
+def perspective(left, right, bottom, top, near, far):
+    P = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+  
+    P[0][0] = 2 * near / (right - left); 
+    P[1][1] = 2 * near / (top - bottom); 
+    P[2][0] = (right + left) / (right - left); 
+    P[2][1] = (top + bottom) / (top - bottom); 
+    P[2][2] = -(far + near) / (far - near); 
+    P[2][3] = -1; 
+    P[3][2] = -2 * far * near / (far - near); 
+    glMultMatrixf(P)
--- main.py
+++ main.py
@@ -5,24 +5,29 @@ from OpenGL.GLU import *
 
 from Transformations import *
 from Models.Cube import *
+from Models.Hall import *
 
-def object(faces, verts):
+def object(edges, verts):
   glBegin(GL_LINES)
-  for i in faces:
+  for i in edges:
     for vertex in i:
-      glVertex3iv(verts[vertex])
+      glVertex3fv(verts[vertex])
   glEnd()
 
 def main():
     pygame.init()
-    display = (500, 500)
+    display = (1000, 720)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL | RESIZABLE)
 
     gluPerspective(40, display[0] / display[1], 1, 10)
-    #glTranslatef(0.0, 0.0, -5)
     translate(0, 0, -5)
-    rotate(45, 0, 1, 0)
+    #rotate(45, 0, 1, 0)
+    #scale(.5)
     i = 0
+    #glOrtho(1, -1, 1, -1, 100, 0.1)
+    #orthogonalProjection(1, -1, 1, -1, 100, 0.1)
+    #glFrustum(1, 10, 1, 10, 1, 10)
+    perspective(1, 10, 1, 10, 1, 10)
     while(1):
         for ev in pygame.event.get():
             if ev.type == pygame.QUIT:
@@ -30,10 +35,10 @@ def main():
                 quit()
         #rotate(1,0,0,1, verts)
         #translate(0,0,1)
-        scale(i)
-        i+=0.25
+        #i+=0.25
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         object(cube_faces, cube_verts)
+        #object(hall_edges, hall_verts)
         pygame.display.flip()
         pygame.time.wait(500)
 
