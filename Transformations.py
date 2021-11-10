fed23dfa8462d9080b60909c1b77a5f229b21c90
brunolobell <brunolobell2@gmail.com>
2021-11-09 22:51:26 -0300

    Adicionando Models .obj

--- Transformations.py
+++ Transformations.py
@@ -43,3 +43,24 @@ def perspective(left, right, bottom, top, near, far):
     P[2][3] = -1; 
     P[3][2] = -2 * far * near / (far - near); 
     glMultMatrixf(P)
+
+def setPixel(x, y):
+    glBegin(GL_LINES)
+    glVertex2i(np.round(x), np.round(y))
+    glEnd()
+
+def bresenham(x1, y1, x2, y2):
+    x = x1
+    y = y1
+    dx = x2 - x1
+    dy = y2 - y1
+    m = dy / dx
+    e = m - (1/2)
+    for i in range(dx):
+        print((x,y))
+        setPixel(x,y)
+        while e > 0:
+            y += 1
+            e -= 1
+        x += 1
+        e += m 
--- main.py
+++ main.py
@@ -3,14 +3,18 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
+from objLoader import *
 from Transformations import *
 from Models.Cube import *
 from Models.Hall import *
 from Models.Test import *
+from Models.Cone import *
+from Models.Cube2 import *
 
 def object(edges, verts):
   glBegin(GL_LINES)
   for i in edges:
+    glColor3fv((10, 100, 1))
     for vertex in i:
       glVertex3fv(verts[vertex])
   glEnd()
@@ -20,14 +24,18 @@ def main():
     display = (1000, 720)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL | RESIZABLE)
 
-    gluPerspective(40, display[0] / display[1], 1, 10)
-    translate(0, 0, -8)
-    #rotate(58, 1, 0, 0)  #58 fica pro meu lado, 61 fica legal tambem
-    scale(0.03)
+    gluPerspective(90, display[0] / display[1], 1, 100) 
+    #translate(0, 0, -8)
+    #rotate(45, 0, 1, 0)  #58 fica pro meu lado, 61 fica legal tambem
+    #scale(0.1)
     i = 0
     #orthogonalProjection(1, -1, 1, -1, 100, 0.1)
     #perspective(1, 10, 1, 10, 1, 10)
-
+    tx, ty = (0,0)
+    zpos = 5
+    obj = OBJ("Models/C3.obj", swapyz=True)
+    translate(tx/20., ty/20., - zpos)
+    scale(0.1)
     while(1):
         for ev in pygame.event.get():
             if ev.type == pygame.QUIT:
@@ -38,10 +46,14 @@ def main():
         #translate(0,0,1)
         #i+=0.25
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        #bresenham(0, 0, 350, 350)
         #object(cube_faces, cube_verts)
         #object(hall_faces, hall_verts)
-        object(test_faces, test_verts)
-
+        #object(cone_faces, cone_verts)
+        #object(cube2_faces, cube2_verts)
+        rotate(0.5, 1, 0, 0)
+        #rotate(1, 0, 1, 0)
+        glCallList(obj.gl_list)
         pygame.display.flip()
         pygame.time.wait(500)
 
